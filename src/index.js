904d254f05689d551596cd57f54e170f58b5c7d0
Florian Hehlen <florian.hehlen@bluewin.ch>
2018-10-13 01:44:33 +0200

    temporarily dissabling features

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ module.exports = async function (source) {
 	const cb = this.async();
 
 	const {style, template, script} = parser(source);
-	const isPostCSSEnable = style.attrs.find(item => item.name === 'lang') || {};
-	const css = isPostCSSEnable.value === 'postcss' ? await postcssrc()
-		.then(({plugins, options}) =>
-			postcss(plugins).process(style.value, {...options, from: this.resourcePath}))
-		.then(res => res.css)
-		.catch(error => cb(error)) : style.value;
+	// const isPostCSSEnable = style.attrs.find(item => item.name === 'lang') || {};
+	// const css = isPostCSSEnable.value === 'postcss' ? await postcssrc()
+	// 	.then(({plugins, options}) =>
+	// 		postcss(plugins).process(style.value, {...options, from: this.resourcePath}))
+	// 	.then(res => res.css)
+	// 	.catch(error => cb(error)) : style.value;
 
 	// const templateString = `<style>${css}</style>${template?template.value:""}`;
-	// const enriched = enrich(script.value, templateString);
+	const enriched = enrich(script.value, null);
 
 	debug(enriched);
 
